b5710a3d5a358d4695b3e7cc19391f0674585aaf
JeffersonToledo <abelktoledo@gmail.com>
2021-04-30 22:42:23 -0300

    Jeff mudei para jeff index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script src="https://kit.fontawesome.com/512b1b950e.js"></script>
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Hotel Mahal2</title>
+    <title>Hotel Mahal Jeff</title>
 </head>
 
 <body>
